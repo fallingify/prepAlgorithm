2b0bc79fbca1fbf054ac5557077e3f443ed4c994
fallingify <gnswldl000@gmail.com>
2024-09-12 01:00:26 +0900

    021

--- 015/015.cpp
+++ 015/015.cpp
@@ -4,6 +4,10 @@
 using namespace std;
 
 int main(){
+  ios::sync_with_stdio(false); 
+  cin.tie(NULL);
+  cout.tie(NULL); 
+  
   int n; cin>>n; 
   vector<int> v(n,0); 
   for(int i=0; i<n; i++) {
